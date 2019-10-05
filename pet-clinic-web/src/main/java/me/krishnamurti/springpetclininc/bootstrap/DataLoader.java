f21abc5520e4c5b80b5612e4dbafb125fcaa2a7f
krishna916 <krishna916@outlook.com>
2019-10-05 23:59:56 +0530

    Auto generation ids

--- pet-clinic-web/src/main/java/me/krishnamurti/springpetclininc/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/me/krishnamurti/springpetclininc/bootstrap/DataLoader.java
@@ -43,14 +43,12 @@ public class DataLoader implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("Owner1");
 		owner1.setLastName("lastName");
 		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("Owner2");
 		owner2.setLastName("lastName2");
 		
@@ -58,14 +56,12 @@ public class DataLoader implements CommandLineRunner{
 		System.out.println("Loaded Owners...");
 		
 		Vet vet1= new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("vet1");
 		vet1.setLastName("lastname");
 		
 		vetService.save(vet1);
 		
 		Vet vet2= new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("vet2");
 		vet2.setLastName("lastname2");
 		
