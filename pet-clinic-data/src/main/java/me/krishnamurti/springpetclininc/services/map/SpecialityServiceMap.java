f98a44f407e880068650983a6b35a5ff443cd403
Krishnamurti Pandaram <krishna916@outlook.com>
2019-10-12 20:43:35 +0530

    Added SpecialityService and Service map (#16)

---
+++ pet-clinic-data/src/main/java/me/krishnamurti/springpetclininc/services/map/SpecialityServiceMap.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package me.krishnamurti.springpetclininc.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import me.krishnamurti.springpetclininc.model.Speciality;
+import me.krishnamurti.springpetclininc.services.SpecialityService;
+
+/**
+ * @author krishna
+ *
+ */
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		super.delete(object);
+	}
+
+}
--- pet-clinic-data/src/main/java/me/krishnamurti/springpetclininc/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/me/krishnamurti/springpetclininc/services/map/VetServiceMap.java
@@ -7,7 +7,9 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import me.krishnamurti.springpetclininc.model.Speciality;
 import me.krishnamurti.springpetclininc.model.Vet;
+import me.krishnamurti.springpetclininc.services.SpecialityService;
 import me.krishnamurti.springpetclininc.services.VetService;
 
 /**
@@ -17,27 +19,48 @@ import me.krishnamurti.springpetclininc.services.VetService;
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
+	private final SpecialityService specialityService;
+
+	/**
+	 * @param specialityService
+	 */
+	public VetServiceMap(SpecialityService specialityService) {
+		super();
+		this.specialityService = specialityService;
+	}
+
 	@Override
 	public Vet save(Vet object) {
+
+		if (object != null) {
+			if (object.getSpecialities().size() > 0) {
+				object.getSpecialities().forEach(speciality -> {
+					if (speciality.getId() == null) {
+						Speciality savedSpeciality = specialityService.save(speciality);
+						speciality.setId(savedSpeciality.getId());
+					}
+				});
+			}
+		}
 		return super.save(object);
 	}
-	
+
 	@Override
 	public Set<Vet> findAll() {
 		return super.findAll();
 	}
-	
+
 	@Override
 	public Vet findById(Long id) {
 		return super.findById(id);
 	}
-	
-	
+
 	@Override
 	public void delete(Vet object) {
-		super.delete(object);;
+		super.delete(object);
+		;
 	}
-	
+
 	@Override
 	public void deleteById(Long id) {
 		super.deleteById(id);
